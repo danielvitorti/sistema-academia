848c48aaa89124b7e6300bdd1de09b6043fdbef1
Daniel Vitor Correa Mendes <daniel.mendes@unimedvr.com.br>
2018-10-29 18:07:25 -0300

    Primeiro commit

---
+++ sistema-academia/Models/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sistema_academia.Models
+{
+    public class Aluno
+    {
+        public int id { get; set; }
+
+        public string nome { get; set; }
+
+        
+    }
+}
